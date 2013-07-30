8a3cfb0b5bf6f2033728bf66585d8716c380d6e5
José L. Patiño <jose.lpa@gmail.com>
2013-07-30 12:19:22 +0200

    Updated ``find_template_path()`` function to make it work.

--- easymode/utils/template.py
+++ easymode/utils/template.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.template import TemplateDoesNotExist
 from django.template.loader import template_source_loaders, find_template_loader
 
+
 def find_template_path(name):
     """
-    Same as :func:`django.template.loader.find_template`, but it only returns the path
-    to the template file.
+    Same as :func:`django.template.loader.find_template`, but it only returns
+    the path to the template file.
     """
     global template_source_loaders
     if template_source_loaders is None:
@@ -17,9 +18,9 @@ def find_template_path(name):
         template_source_loaders = tuple(loaders)
     for loader in template_source_loaders:
         try:
-            _, display_name = loader(name)
-            if display_name:
-                return display_name
+            template, _ = loader(name)
+            if template:
+                return template.nodelist[0].source[0].name
         except TemplateDoesNotExist:
             pass
     raise TemplateDoesNotExist(name)
